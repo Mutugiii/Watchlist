9b1746bc5f2b1620244f92a1d98eb736aa781d46
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-29 14:33:13 +0300

    Connect our app to the new config file(for api)

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 from .config import DevConfig
 
 # Initialize the application
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config = True)
 
 # Setting up Configuration
 app.config.from_object(DevConfig)
+app.config.from_pyfile('config.py')
 
 # Importing views to allow us to create views
 from app import views   
