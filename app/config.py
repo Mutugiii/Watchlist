0da11064782175da6ab8f6dcc4c3cd6f81f237c6
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-01-29 14:31:55 +0300

    Add the base url for api calls to the config file

--- app/config.py
+++ app/config.py
@@ -1,7 +1,8 @@
 class Config:
     '''
     General configuration parent class.
-    '''
+   '''
+    MOVIE_API_BASE_URL = 'https://api.themoviedb.org/3/movie/{}?api_key={}'
 
 class ProdConfig(Config):
     '''
