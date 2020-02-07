4c899db79b4432f342cce06e8c528508e0cef8f5
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-07 14:03:10 +0300

    Correct import

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,4 +1,4 @@
-import flask_wtf from FlaskForm
+from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, ValidationError
 from wtforms.validators import Required, Email, EqualTo
 from ..models import User
